f746420a639ce614703d858e0f34ea338b4acf68
Aditya <aditya.murthy@asu.edu>
2016-08-09 18:48:46 -0700

    Commit 2 - View all files in ListView

--- app/src/main/java/edu/asu/msse/anmurth1/sdkscanner/ProgressActivity.java
+++ app/src/main/java/edu/asu/msse/anmurth1/sdkscanner/ProgressActivity.java
@@ -1,7 +1,12 @@
 package edu.asu.msse.anmurth1.sdkscanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -9,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Copyright 2015 Aditya Narasimhamurthy.
@@ -30,32 +36,51 @@ import java.io.IOException;
  */
 public class ProgressActivity extends MainActivity {
 
+    ListView lv;
+    String[] items;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result1);
+        //setContentView(R.layout.audio_list);
+        lv = (ListView) findViewById(R.id.listView);
 
-        TextView tv = (TextView) findViewById(R.id.textView);
+        final ArrayList<File> mySongs = findFiles(Environment.getExternalStorageDirectory());
+        items = new String[mySongs.size()];
+        //toast(myVideos.get(i).getName().toString());
+        for(int i=0; i<mySongs.size(); i++)
+            items[i] = mySongs.get(i).getName();
 
-        File dir = Environment.getExternalStorageDirectory();
-        File file = new File(dir, "V.text");
+        ArrayAdapter<String> adp = new ArrayAdapter<>(getApplicationContext(),R.layout.list_layout,R.id.textView2, items);
+        lv.setAdapter(adp);
 
-        if(file.exists()){
-            StringBuilder sb = new StringBuilder();
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                String line;
-                while ((line = br.readLine()) != null) {
-                    sb.append(line + '\n');
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                //startActivity(new Intent(getApplicationContext(),AudioPlayer.class).putExtra("pos", position).putExtra("songslist", mySongs));
             }
-            tv.setText(sb);
+        });
+    }
+    public ArrayList<File> findFiles(File root) {
+        ArrayList<File> a1 = new ArrayList<>();
+        File[] files = root.listFiles();
+        for(File singleFile : files){
+            if(singleFile.isDirectory() && !singleFile.isHidden()){
+                a1.addAll(findFiles(singleFile));
+            }
+            else{
+                if(singleFile.getName().matches(".*")){
+                    a1.add(singleFile);
+                }
+            }
         }
+        return  a1;
 
-        else{
-            tv.setText("No file found");
-        }
+    }
+
+    public void analyzeButton(View view) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
